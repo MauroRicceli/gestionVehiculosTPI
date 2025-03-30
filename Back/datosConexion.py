ea37aa89535e3ea0def5ed9196223911c1f3ea0a
Mauro Andrés Ricceli <116189713+MauroRicceli@users.noreply.github.com>
2025-03-30 13:59:03 -0300

    Update datosConexion.py

--- Back/datosConexion.py
+++ Back/datosConexion.py
@@ -1,10 +1,10 @@
 
 class datosSQL(object):
   def __init__(self):
-    self.host = "b3psdfrcs2ajufhy7cov-mysql.services.clever-cloud.com"
-    self.user = "ujuznvazoa0ixtnl"
-    self.password = "XS3a0sxFPwZgVRr2wm8P"
-    self.database = "b3psdfrcs2ajufhy7cov"
+    self.host = "..."
+    self.user = "..."
+    self.password = "..."
+    self.database = "..."
 
 class datosAPI(object):
   def __init__(self):
